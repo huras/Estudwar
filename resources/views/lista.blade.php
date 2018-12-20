7b69691f45601517df85037d92313eb06be13d98
huras <hurast@gmail.com>
2018-12-19 22:44:08 -0200

    Lista - layout update

--- resources/views/lista.blade.php
+++ resources/views/lista.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts/simple')
 
 @section('content')
+    <h1> Lista </h1>
     @foreach($entries as $entry)
-        <a href='#'> {{$entry->term}} </a>
+        <a href='#'> {{$entry->term}} </a><br>
     @endforeach
 @endsection
